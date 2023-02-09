60cc3e041f58e55be60cbdbf8a26f780f1f66016
Tems <tems@tems.pl>
2023-02-09 21:57:21 +0100

    .

--- gallery/index.php
+++ gallery/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kindergarden</title>
+    <title>Kindergarten</title>
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/gallery.css">
 </head>
