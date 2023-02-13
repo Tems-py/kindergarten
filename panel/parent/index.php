852ff79b33a3b560cd2deca852fb2314f2527570
Tems <tems@tems.pl>
2023-02-13 20:03:26 +0100

    minor code improvements

--- panel/parent/index.php
+++ panel/parent/index.php
@@ -21,21 +21,21 @@
 <body>
     <div id="container">
         <div id="navbar">
-            <div id="buttons">
-                <div id="button"><a href="gallery">Gallery</a></div>
+            <div id="buttons">Ś
+                <div id="button"><a href="../../gallery">Gallery</a></div>
                 <div id="button">Contact</div>
-                <div id="button">Terms and contidions</div>
+                <div id="button">Terms and Conditions</div>
             </div>
             
                 <?php
 
                 if (isset($_SESSION['email'])) {
                     echo '<div id="profile"><img src="https://robohash.org/'.$_SESSION['email'].'?set=set4" alt="">  ';
-                    echo '<a href="panel">Panel</a>  ';
-                    echo '<a href="logout">Logout</a>';
+                    echo '<a href="../panel">Panel</a>  ';
+                    echo '<a href="../../logout">Logout</a>';
                 }
                 else {
-                    echo '<a href="login">Login</a>';
+                    echo '<a href="../../login">Login</a>';
                 }
                 ?>
             </div>
@@ -44,7 +44,7 @@
             <div id="accountInfo">
                 <?php
                     $email = $_SESSION['email'];
-                    $query = mysqli_query($conn, "SELECT * FROM `accounts` WHERE email like '{$email}'");
+                    $query = mysqli_query($conn, "SELECT * FROM `accounts` WHERE email like '$email'");
                     $row = mysqli_fetch_array($query);
                     echo "Welcome: ".$row['name']." ".$row['familyName']."<hr> email: ".$row["email"];
                 ?>
@@ -58,10 +58,10 @@
                     <?php
 
                     $count = 0;
-                    $query = mysqli_query($conn, "SELECT * FROM `accounts` WHERE email like '{$email}'");
+                    $query = mysqli_query($conn, "SELECT * FROM `accounts` WHERE email like '$email'");
                     $row = mysqli_fetch_array($query);
                     $accId = $row['accountId'];
-                    $query = mysqli_query($conn, "SELECT * FROM `children` JOIN relations ON children.id = relations.childId where relations.accountId = '{$accId}'");
+                    $query = mysqli_query($conn, "SELECT * FROM `children` JOIN relations ON children.id = relations.childId where relations.accountId = '$accId'");
                     while($row=mysqli_fetch_array($query)){
                         $count = $count + 1;
                     }
@@ -74,10 +74,10 @@
                 <div id="childInfo">
                     <?php
                         $count = 0;
-                        $query = mysqli_query($conn, "SELECT * FROM `accounts` WHERE email like '{$email}'");
+                        $query = mysqli_query($conn, "SELECT * FROM `accounts` WHERE email like '$email'");
                         $row = mysqli_fetch_array($query);
                         $accId = $row['accountId'];
-                        $query = mysqli_query($conn, "SELECT * FROM `children` JOIN relations ON children.id = relations.childId where relations.accountId = '{$accId}'");
+                        $query = mysqli_query($conn, "SELECT * FROM `children` JOIN relations ON children.id = relations.childId where relations.accountId = '$accId'");
                         while($row=mysqli_fetch_array($query)){
                             $count = $count + 1;
                             echo "Name: ".$row['name']." ".$row['familyName']."<hr class='child'> birthdate: ".$row["birthdate"]."<hr class='child'> Group Id: ".$row["groupId"]."<hr>";
@@ -90,11 +90,11 @@
             <div id="payments">
                         <?php
                        
-                        $query = mysqli_query($conn, "SELECT * FROM `children` JOIN relations ON children.id = relations.childId where relations.accountId = '{$accId}'");
+                        $query = mysqli_query($conn, "SELECT * FROM `children` JOIN relations ON children.id = relations.childId where relations.accountId = '$accId'");
                         while($row=mysqli_fetch_array($query)){
                             echo "<h1> Child: ".$row['name']." ".$row['familyName']."</h1>";
                             $id = $row['id'];
-                            $query2 = mysqli_query($conn, "SELECT * FROM `paymentnotice` WHERE childId = {$id}");
+                            $query2 = mysqli_query($conn, "SELECT * FROM `paymentnotice` WHERE childId = $id");
                             while($row2=mysqli_fetch_array($query2)){
                                 echo "Payment due to: ".$row2['dateDue']."<hr class='child1'> Cost: ".$row2["cost"]."<hr class='child1'> Objectives: ".$row2["objectives"];
                                 echo "<hr class='child'>";
