7c0cd0933566129784c1a538f3ae2fe225a47ffe
Krzychu <redshadowspy1@wp.pl>
2023-02-20 13:35:35 +0100

    fixed register... again

--- register/index.php
+++ register/index.php
@@ -31,11 +31,7 @@
                 
                 if(isset($_POST['email']) && isset($_POST['password']) && isset($_POST['repeatPassword'])){
                     if($_POST['password']==$_POST['repeatPassword']){
-<<<<<<< HEAD
                         $query = mysqli_query($conn, "INSERT INTO `accounts`(`email`, `name`, `familyName`, `accountType`, `password`) VALUES ('{$_POST['email']}','{$_POST['name']}','{$_POST['surname']}','parent','{$_POST['password']}');");
-=======
-                        $query = mysqli_query($conn, "INSERT INTO `accounts`(`email`, `accountType`, `password`,`name`,`familyName`) VALUES ('{$_POST['email']}','parent','{$_POST['password']}'),'{$_POST['name']}','{$_POST['surname']}'");
->>>>>>> adf12b14d0d2de85b92eeaae5ac9da097a5b3320
                         echo "Registration successful!";
                     }
                     else{
