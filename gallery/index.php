561ea261f9fa3d2fb03acead83336b2e58be5698
tems <tems@tems.pl>
2023-02-20 14:04:05 +0100

    Add favicon

--- gallery/index.php
+++ gallery/index.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kindergarten</title>
+    <title>Kindergarten</title><link rel='icon' type='image/x-icon' href='../img/logo.svg'>
     <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="../css/gallery.css">
 </head>
