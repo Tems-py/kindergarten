a531632fa04d81ac572ea5511d3513bc1728d332
Krzychu <redshadowspy1@wp.pl>
2023-02-23 18:12:08 +0100

    Fixed payments in parent

--- panel/parent/index.php
+++ panel/parent/index.php
@@ -70,15 +70,22 @@
             </div>
             <div id="payments">
                         <?php
-                       
+                        $query = mysqli_query($conn, "SELECT * FROM `accounts` WHERE email like '$email'");
+                        $row = mysqli_fetch_array($query);
+                        $accId = $row['accountId'];
                         $query = mysqli_query($conn, "SELECT * FROM `children` JOIN relations ON children.id = relations.childId where relations.accountId = '$accId'");
                         while($row=mysqli_fetch_array($query)){
                             echo "<h1> Child: ".$row['name']." ".$row['familyName']."</h1>";
                             $id = $row['id'];
                             $query2 = mysqli_query($conn, "SELECT * FROM `paymentnotice` WHERE childId = $id");
+                            $count = 0;
                             while($row2=mysqli_fetch_array($query2)){
+                                $count = $count + 1;
                                 echo "Payment due to: ".$row2['dateDue']."<hr class='child1'> Cost: ".$row2["cost"]."<hr class='child1'> Objectives: ".$row2["objectives"];
                                 echo "<hr class='child'>";
+                            }
+                            if($count<=0){
+                                echo "N/A";
                             }
                             echo "<hr>";
                         }
